22b607de97a759dd93b62c1b42e8f74e62119452
Caio lima <caiohenrys50@gmail.com>
2024-03-12 23:38:07 -0300

    feat:  image loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import ImgAstronautSaturn from "../assets/astronauta-sentado-no-planeta (1).png";
 import { useEffect, useState } from "react";
+import ImgAstronautDark from "../assets/astronauta noite.png";
 
 export default function Loading() {
   const [isLoading, setIsLoading] = useState(true);
@@ -18,14 +19,24 @@ export default function Loading() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       {isLoading && (
-        <Image
-          src={ImgAstronautSaturn}
-          alt="ASTRONAUTA-SATURNO"
-          width={140}
-          height={140}
-          loading="lazy"
-          className="animate-bounce"
-        />
+        <>
+          <Image
+            src={ImgAstronautSaturn}
+            alt="ASTRONAUTA-SATURNO"
+            width={140}
+            height={140}
+            loading="lazy"
+            className="animate-bounce flex dark:hidden"
+          />
+          <Image
+            src={ImgAstronautDark}
+            alt="ASTRONAUTA-NOITE-PC"
+            width={120}
+            height={120}
+            loading="lazy"
+            className="animate-bounce hidden dark:flex"
+          />
+        </>
       )}
     </div>
   );
